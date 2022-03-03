3ccb858efbb5f34d615dbb52b57620399d6285ea
kotla <kotlavenu27@gmail.com>
2022-03-03 16:39:21 +0530

    update

--- ecetest/src/ecetest/testclass.java
+++ ecetest/src/ecetest/testclass.java
@@ -5,6 +5,7 @@ public class testclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("venu");
 	}
 
 }
